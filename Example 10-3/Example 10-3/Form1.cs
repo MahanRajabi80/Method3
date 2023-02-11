649e008b1fd773f55c77f264a15b95f2425e37c5
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 17:51:00 +0330

    Add files via upload

---
+++ Example 10-3/Example 10-3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_10_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------
+        private Label RandomLabel()
+        {
+            Label l = new Label();
+            Random r = new Random(DateTime.Now.Millisecond);
+            l.Left = r.Next(this.Width);
+            l.Top = r.Next(this.Height);
+            l.Width = r.Next(50);
+            l.Height = r.Next(50);
+            l.Text = "";
+            l.BorderStyle = BorderStyle.FixedSingle;
+            l.BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+            return l;
+        }
+        //------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(RandomLabel());
+            timer1.Interval = 10;
+            timer1.Start();
+        }
+        //------------------------------------------------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Controls.Add(RandomLabel());
+        }
+    }
+}
